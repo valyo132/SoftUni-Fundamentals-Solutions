1944257992f705cdcad2a3cb77dd5c811e189b53
valyo132 <valyo132@abv.bg>
2022-07-20 21:42:56 +0300

    Regular Expressions

---
+++ Regular Expressions/01. Match Full Name/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _01._Match_Full_Name
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string names = Console.ReadLine();
+
+            string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+";
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matches = regex.Matches(names);
+
+            Console.WriteLine(string.Join(" ", matches));
+        }
+    }
+}
